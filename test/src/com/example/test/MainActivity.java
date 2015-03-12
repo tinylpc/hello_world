c1b68c66b5c8b74b9c3bc418401b70efe0d45c8a
tinylpc <lpc19900204@gmail.com>
2015-03-12 14:15:22 +0800

    add test folder

---
+++ test/src/com/example/test/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity
+{
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void onDestroy()
+	{
+		super.onDestroy();
+	}
+
+}
---
+++ test/src/com/example/test/MyApp.java
@@ -0,0 +1,14 @@
+package com.example.test;
+
+import android.app.Application;
+
+public class MyApp extends Application
+{
+
+	@Override
+	public void onCreate()
+	{
+		super.onCreate();
+	}
+
+}
